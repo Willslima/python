ccf0d6bd61c8ff940a5032fdafedb5ced32e873e
Willslima <williansantos38@gmail.com>
2023-05-26 17:33:57 -0300

    location of changed files

---
+++ aula_04.py
@@ -0,0 +1,20 @@
+a = 10
+b = 8
+
+resultado = a - b
+print("O resultado da operacao de soma é: ", resultado)
+
+resultado2 = a / b
+print("O resultado da operacao de divisão é: ", resultado2)
+
+resultado3 = a // b
+print("O resultado da operação de inteiro é: " , resultado3)
+
+resultado4 = a % b
+print("O resultado da operação de módulo é: ", resultado4)
+
+resultado5 = a * b
+print("O resultado da multiplicação é: ", resultado5)
+
+resultado6 = a**b
+print('O resultado é: ', resultado6)
---
+++ teste.py
@@ -0,0 +1,5 @@
+print('Usando python')
+print(type('u')) #Str
+print(type(1)) #Int
+print(type(0.2)) #Float
+print(type(False)) #bool
