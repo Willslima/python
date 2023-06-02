c33e16f03e5e989f25c017b5d057173ee98cda89
Willslima <williansantos38@gmail.com>
2023-06-02 12:44:31 -0300

    building an api

--- api/server.py
+++ api/server.py
@@ -3,11 +3,32 @@ port = 8000
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type','text/html; charset=utf-8')
-        self.send_header('Teste','abc')
-        self.end_headers()
-        self.wfile.write('Olá, mundo!'.encode())
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-Type','text/html; charset=utf-8')
+            self.end_headers()
+            data = f''' 
+            <html>
+                <head>
+                    <title>Olá mundo !</title>
+                </head>
+                <body>
+                    <p>Server testing!</p>
+                    <p>Directory: {self.path} </p>
+                </body>
+            </html>
+            '''
+            self.wfile.write(data.encode())
+        elif self.path == '/events':
+            self.send_response(200)
+            self.send_header('Content-Type','text/html;charset=utf-8')
+            self.end_headers() 
+            data = f'''
+            <html>
+                <p>Oi</p>
+            </html>
+            '''
+            self.wfile.write(data.encode())
 
 server = HTTPServer(('localhost', port), SimpleHandler)
 server.serve_forever()
